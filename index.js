9a7b5ed41184dc494fcea50c452445e29b5aac0b
peachparfa1t <filatova7891@gmail.com>
2025-06-19 15:21:59 +0800

    commit 1

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' }); 
+        res.end('<h1>привет, Октагон!</h1>');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' }); 
+        res.end('Страница не найдена');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Сервер запущен на http://localhost:3000/');
+});
+
