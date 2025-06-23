872cfb1fe68ead9a94eca8849935b2ba57636e27
PeachParfa1t <filatova7891@gmail.com>
2025-06-23 16:56:31 +0800

    5 задание, обновление команд

--- bot.js
+++ bot.js
@@ -8,3 +8,21 @@ const bot = new TelegramBot(token, { polling: true });
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, 'Привет, октагон!');
 });
+
+bot.onText(/\/help/, (msg) => {
+  const text = `
+Доступные команды:
+/help - список команд
+/site - сайт Октагона
+/creator - информация о создателе
+  `;
+  bot.sendMessage(msg.chat.id, text);
+});
+
+bot.onText(/\/site/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Сайт Октагона: https://octagon.org.ru');
+});
+
+bot.onText(/\/creator/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Создатель: Филатова Софья Сергеевна'); 
+});
