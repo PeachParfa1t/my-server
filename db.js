2e8c40780ca53160155129f5364dcabbecb3af78
PeachParfa1t <filatova7891@gmail.com>
2025-06-26 16:13:46 +0800

    Исправлена ручная обработка на app во 2, 3, 4, 5, 6 заданиях

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
+const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'ChatBotTests',
 });
 
-module.exports = pool.promise();
+module.exports = db;
--- index.js
+++ index.js
@@ -1,57 +1,66 @@
-const http = require('http');
-const url = require('url');
+const http = require('express');
+const app = express();
 const db = require('./db');
 
-const server = http.createServer(async (req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  const path = parsedUrl.pathname;
-  const query = parsedUrl.query;
+const port = 3000;
 
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+pp.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-  if (path === '/getAllItems' && req.method === 'GET') {
-    const [rows] = await db.query('SELECT * FROM Items');
-    res.end(JSON.stringify(rows));
-  }
-
-  else if (path === '/addItem' && req.method === 'POST') {
-    const { name, desc } = query;
-    if (!name || !desc) return res.end('null');
-
-    await db.query('INSERT INTO Items (name, `desc`) VALUES (?, ?)', [name, desc]);
-    const [newItem] = await db.query('SELECT * FROM Items ORDER BY id DESC LIMIT 1');
-    res.end(JSON.stringify(newItem[0]));
-  }
-
-  else if (path === '/deleteItem' && req.method === 'POST') {
-    const { id } = query;
-    if (!id || isNaN(id)) return res.end('null');
-
-    const [oldItem] = await db.query('SELECT * FROM Items WHERE id = ?', [id]);
-    if (oldItem.length === 0) return res.end('{}');
-
-    await db.query('DELETE FROM Items WHERE id = ?', [id]);
-    res.end(JSON.stringify(oldItem[0]));
-  }
-
-  else if (path === '/updateItem' && req.method === 'POST') {
-    const { id, name, desc } = query;
-    if (!id || isNaN(id) || !name || !desc) return res.end('null');
-
-    const [oldItem] = await db.query('SELECT * FROM Items WHERE id = ?', [id]);
-    if (oldItem.length === 0) return res.end('{}');
-
-    await db.query('UPDATE Items SET name = ?, `desc` = ? WHERE id = ?', [name, desc, id]);
-    const [updatedItem] = await db.query('SELECT * FROM Items WHERE id = ?', [id]);
-    res.end(JSON.stringify(updatedItem[0]));
-  }
-
-  else {
-    res.writeHead(404);
-    res.end(JSON.stringify({ error: 'Not Found' }));
+app.get('/getAllItems', async (req, res) => {
+  try {
+  const [rows] = await db.query('SELECT * FROM Items');
+  res.json(rows);
+}catch (err) {
+    res.status(500).json({ error: 'DB Error' });
   }
 });
 
-server.listen(3000, () => {
-  console.log('Сервер запущен на http://localhost:3000');
+app.post('/addItem', async (req, res) => {
+  const { name, desc } = query;
+  if (!name || !desc) return res.json('null');
+  try {
+    await db.query('INSERT INTO Items (name, `desc`) VALUES (?, ?)', [name, desc]);
+    const [newItem] = await db.query('SELECT * FROM Items ORDER BY id DESC LIMIT 1');
+    res.json(newItem[0]);
+  }catch (err) {
+    res.status(500).json({ error: 'DB Insert Error' });
+  }
+});
+
+app.post('/deleteItem', async (req, res) => {
+  const { id } = req.query;
+  if (!id || isNaN(id)) return res.json('null');
+try{
+  const [oldItem] = await db.query('SELECT * FROM Items WHERE id = ?', [id]);
+  if (oldItem.length === 0) return res.json('{}');
+
+  await db.query('DELETE FROM Items WHERE id = ?', [id]);
+  res.json(oldItem[0]);
+} catch (err) {
+    res.status(500).json({ error: 'DB Delete Error' });
+}
+});
+
+app.post('/updateItem', async (req, res) => {
+  const { id, name, desc } = query;
+  if (!id || isNaN(id) || !name || !desc) return res.json('null');
+  try {
+    const [oldItem] = await db.query('SELECT * FROM Items WHERE id = ?', [id]);
+    if (oldItem.length === 0) return res.json('{}');
+
+    await db.query('UPDATE Items SET name = ?, `desc` = ? WHERE id = ?', [name, desc, id]);
+    const [updatedItem] = await db.query('SELECT * FROM Items WHERE id = ?', [id]);
+    res.json(updatedItem[0]);
+  } catch (err) {
+    res.status(500).json({ error: 'DB Update Error' });
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на http://localhost:${port}`);
 });
